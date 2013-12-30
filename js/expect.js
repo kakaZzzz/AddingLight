9d7dc9d87dd2d7ad3d23651797c153739a4944e5
Aling <aling@Aling-MacBook.local>
2013-12-30 17:45:14 +0800

    modify date input

--- js/expect.js
+++ js/expect.js
@@ -4,4 +4,13 @@ define(function(require, exports, module){
             tmpl = '您的预产期是：<span class="red">{{expect}}</span><br/>您已经怀孕：<span class="red">{{week}}</span><br/>离宝宝出生还有：<span class="red">{{date}}</span>';
         $('.expect_main .ret').html(Mustache.render(tmpl, {expect: '2014年7月25日', week: '10周3天', date: '208天'}));
     });
+    if($.os.ios){//如果是ios系统，需要处理date的宽度
+        function resize(){
+            //$('menstrual_date').width($('#menstrual_cycle').width());
+            document.getElementById('menstrual_date').style.width = 
+            document.getElementById('menstrual_cycle').offsetWidth - 15 + 'px';
+        }
+        $(window).on('orientationchange', resize);
+        resize();
+    }
 });
